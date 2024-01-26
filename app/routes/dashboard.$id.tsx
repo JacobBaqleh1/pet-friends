77c4b0b32cb6acab8a78ec90c4ca6d2723771a4c
Jacob Baqleh <baqlehjacob1@gmail.com>
2024-01-25 18:50:05 -0800

    starting session flash

--- app/routes/dashboard.$id.tsx
+++ app/routes/dashboard.$id.tsx
@@ -1,8 +1,16 @@
+import { LoaderFunctionArgs } from "@remix-run/node";
+
+// export async function loader(){
+
+// }
 export default function Component() {
   return (
     <div>
       <h2>Animal ID PAGE!!</h2>
-      <img src="https://images.unsplash.com/photo-1517849845537-4d257902454a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZG9nfGVufDB8fDB8fHww" alt="animal" />
+      <img
+        src="https://images.unsplash.com/photo-1517849845537-4d257902454a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZG9nfGVufDB8fDB8fHww"
+        alt="animal"
+      />
     </div>
-  )
+  );
 }
--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,82 +1,95 @@
-import {  type ActionFunctionArgs } from "@remix-run/node";
+import { type ActionFunctionArgs } from "@remix-run/node";
 import { Form, Link, Outlet, useActionData } from "@remix-run/react";
 import { json } from "@remix-run/node";
-export async function action({request}: ActionFunctionArgs){
+export async function action({ request }: ActionFunctionArgs) {
   //getting the form data
   let formData = await request.formData();
   let newUser = {
-    zipcode: formData.get('zipcode'),
-    pet: formData.get('pet')
+    zipcode: formData.get("zipcode"),
+    pet: formData.get("pet"),
   };
   //fetching api token from the petfinder website
   const response = await fetch("https://api.petfinder.com/v2/oauth2/token", {
     method: "POST",
     body: new URLSearchParams({
       grant_type: "client_credentials",
-       client_id: "0b5CWcWxxaW3fXPw7Lh2p0qMX9fpaYpOctVBLwbT3V4q2ift7I",
-       client_secret: "un5wi6EsUOmgy0qwvsfHTofWlXL7Pboo780HRHFS",
-     }),
-   });
-   const tokenData = await response.json();
-   //fetching api data using the form data and the api token
-   const res = await fetch(
-     `https://api.petfinder.com/v2/animals?location=${newUser.zipcode}&type=${newUser.pet}`,
-     {
-       headers: {
-         Authorization: `Bearer ${tokenData.access_token}`,
-       },
-     }
-   );
-   const animalData = await res.json();
-   
-   return json(animalData)
-}
+      client_id: "0b5CWcWxxaW3fXPw7Lh2p0qMX9fpaYpOctVBLwbT3V4q2ift7I",
+      client_secret: "un5wi6EsUOmgy0qwvsfHTofWlXL7Pboo780HRHFS",
+    }),
+  });
+  const tokenData = await response.json();
+  //fetching api data using the form data and the api token
+  const res = await fetch(
+    `https://api.petfinder.com/v2/animals?location=${newUser.zipcode}&type=${newUser.pet}`,
+    {
+      headers: {
+        Authorization: `Bearer ${tokenData.access_token}`,
+      },
+    }
+  );
+  const animalData = await res.json();
 
+  return json(animalData);
+}
 
 export default function Component() {
   //retrieve action function data
   const data = useActionData<typeof action>();
-  console.log(data?.animals)
+  console.log(data?.animals);
   return (
-    <main>
-      <Form method="post">
-        <h2>Enter your zipcode to get started</h2>
-        <fieldset>
-          <input
-            type="text"
-            name="zipcode"
-            placeholder="Enter Zipcode..."
-            className="w-full border border-gray-300 rounded-lg px-4 py-2 mt-2"
-          />
-          <input type="radio" name="pet" value='dog' />Dogs
-          <input type="radio" name="pet" value='cat' />Cats
+    <>
+      {" "}
+      <main>
+        <Form method="post" className="w-full max-w-lg">
+          <div>
+            <h2>Enter your zipcode to get started</h2>
+
+            <input
+              type="text"
+              name="zipcode"
+              placeholder="Enter Zipcode..."
+              className="w-full border border-gray-300 rounded-lg px-4 py-2 mt-2"
+            />
+          </div>
+          <input type="radio" name="pet" value="dog" />
+          Dogs
+          <input type="radio" name="pet" value="cat" />
+          Cats
           <br />
           <button type="submit">Search</button>
-        </fieldset>
-      </Form>
-      
-      <div className="grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-8">
-        
-        {data?.animals ? (
-        data?.animals.map((animal): any => (
-          <div key={animal.id} className="flex flex-col overflow-hidden rounded-lg border bg-white">
-          <Link prefetch='intent' className="group relative block h-48 overflow-hidden bg-gray-100 md:h-64"
-           to={`/dashboard/${animal.id}`}>
-          <img alt="animal" src='https://images.unsplash.com/photo-1517849845537-4d257902454a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZG9nfGVufDB8fDB8fHww'></img>
-          </Link>
-          </div>
-        ))
-        ) :( <p></p>
-      )
-      }
-        
-      </div>
-      <Outlet />
-    </main>
+        </Form>
+
+        <div className="grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-8">
+          {data?.animals ? (
+            data?.animals.map((animal): any => (
+              <div
+                key={animal.id}
+                className="flex flex-col overflow-hidden rounded-lg border bg-white"
+              >
+                <Link
+                  prefetch="intent"
+                  className="group relative block h-48 overflow-hidden bg-gray-100 md:h-64"
+                  to={`/dashboard/${animal.id}`}
+                >
+                  <img
+                    alt="animal"
+                    src="https://images.unsplash.com/photo-1517849845537-4d257902454a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZG9nfGVufDB8fDB8fHww"
+                  ></img>
+                </Link>
+              </div>
+            ))
+          ) : (
+            <p></p>
+          )}
+        </div>
+        <Outlet />
+      </main>
+    </>
   );
 }
 
-{/* <form className="w-full max-w-lg">
+{
+  /* <form className="w-full max-w-lg">
   <div className="flex flex-wrap -mx-3 mb-6">
     <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
       <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
@@ -130,4 +143,5 @@ export default function Component() {
       <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="90210" />
     </div>
   </div>
-</form> */}
+</form> */
+}
